8bcb6bddc1994d080dd24c538f85d883e580ee61
Melkzedk <melkywafula29@gmail.com>
2025-11-03 10:28:03 +0300

    added navbar.js file

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">    
+        <h1>My Application</h1>
+        <ul className="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li> 
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+  );
+}
