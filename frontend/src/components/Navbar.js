49b8e291d321746dd9b8bc9576055339b6bccd3f
Melkzedk <melkywafula29@gmail.com>
2025-11-05 13:58:36 +0300

    small changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,6 @@ function Navbar() {
         <h1>My Application</h1>
         <ul className="nav-links">
             <li><a href="/">Home</a></li>
-            <li><a a href="/">Account</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/about">About</a></li> 
-            <li><a href="/contact">Contact</a></li>
         </ul>
     </nav>
   );
